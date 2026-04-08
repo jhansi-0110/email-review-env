4663eaf5c9546b073b03db5ef5624903352daf4c
Yarlagadda Jhansi Lakshmi <JhansiY1@users.noreply.huggingface.co>
2026-04-08 15:52:24 +0000

    Update server/app.py

--- server/app.py
+++ server/app.py
@@ -21,3 +21,11 @@ app = create_app(
     EmailObservation,
     env_name="email_review_env"
 )
+
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
